26a18dccc7457e1afddd73d77f4f433acc67f695
jeevana-priya <55318388+jeevana-priya@users.noreply.github.com>
2024-12-07 18:51:05 -0500

    Add files via upload

---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+export default function Main(){
+    return (
+        <main className="main">
+            <h1>Fun facts about React</h1>
+            <ul>
+                <li>was first</li>
+                <li>was orginally</li>
+                <li>Has well</li>
+                <li>is mainatied</li>
+                <li>power</li>
+            </ul>
+        </main>
+       
+    )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+
+export default function Navbar(){
+    return(
+        <header>
+        <nav className = "class-Navbar">
+            <img className="img-logo" src =  '/logo192.png' alt="logo" ></img>
+            <span>RaectFacts</span>
+        </nav>
+    </header>
+    )
+}
+ 
